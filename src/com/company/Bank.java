9998bb3587b194a2dd732f5da3e652c01284afaf
Michel Abu Nassar <michel.abu.n@gmail.com>
2021-11-22 21:48:24 +0200

    new

--- src/com/company/Bank.java
+++ src/com/company/Bank.java
@@ -3,54 +3,65 @@ package com.company;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Bank<myList> implements IBank {
+public class Bank<myList> extends StandardAccount implements IBank {
 
     List<IAccount> myList = new ArrayList<IAccount>();
 
-    public Bank() { }
-
+    public Bank(int accountNumber, double creditLimit) {
+        super(accountNumber, creditLimit);
+    }
 
     @Override
     public void OpenAccount(IAccount account) {
+
         myList.add(account);
     }
 
 
     @Override
     public void CloseAccount(int accountNumber) {
-        if (myList.contains(accountNumber) && (balance> 0)) {
+        if (myList.contains(accountNumber) && (balance >= 0)) {
 
-                myList.remove(accountNumber);
+            myList.remove(accountNumber);
 
-            }else if (myList.contains(accountNumber) && ( balance < 0 )) {
+        } else if (myList.contains(accountNumber) && (balance < 0)) {
 
-                System.out.println("The Account is not closed due to debt");
-            }
+            System.out.println("The Account is not closed due to debt");
         }
-
+    }
 
     @Override
     public List<IAccount> GetAllAccounts() {
 
-        for(int i=0;i<myList.size();i++);
-         myList.get(account);
+        for (int i = 0; i < myList.size(); i++) ;
+            myList.get(accountNumber);
 
+        return myList;
     }
 
     @Override
-    public List<IAccount> GetAllAccountsInDebt() {
+    public ArrayList<Integer> GetAllAccountsInDebt() {
 
-        for(int i=0;i<myList.size();i++)
-            myList.get( IAccount< 0) ;
+        ArrayList<Integer> GetAllAccountsInDebt = new ArrayList<>();
 
+        for (int i = 0; i < myList.size(); i++)
+            if (balance < 0) ;
+        boolean add = GetAllAccountsInDebt.add(accountNumber);
+        return GetAllAccountsInDebt;
     }
+
 
     @Override
     public List<IAccount> GetAllAccountsWithBalance(double balanceAbove) {
-        if (balanceAbove < accounts)
 
-            return accounts
+        List<IAccount> GetAllAccountsWithBalance = new ArrayList<IAccount>();
 
+        for (int i = 0; i < myList.size(); i++)
+
+        if (balanceAbove*2 < balance)
+
+            GetAllAccountsWithBalance.get(accountNumber);
+        return GetAllAccountsWithBalance;
     }
 
 }
--- src/com/company/IBank.java
+++ src/com/company/IBank.java
@@ -1,12 +1,13 @@
 package com.company;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public interface IBank {
     void OpenAccount(IAccount account);
     void CloseAccount(int accountNumber);
     List<IAccount> GetAllAccounts();
-    List<IAccount> GetAllAccountsInDebt();
+    ArrayList<Integer> GetAllAccountsInDebt();
     List<IAccount> GetAllAccountsWithBalance(double balanceAbove);
 
 
